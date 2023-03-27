7903cc84e417ff2599a4c371696c40acee1c1f49
kmanishankar <106505679+kmanishankar@users.noreply.github.com>
2023-03-27 11:58:04 +0530

    Add files via upload

---
+++ biggestofthreenumbers.py
@@ -0,0 +1,12 @@
+n1=int(input("enter a number1"))
+n2=int(input("enter a number2"))
+n3=int(input("enter a number 3"))
+big=num1
+if(big>num2 and big>num3):
+    print(num1)
+big=num2
+else if(big>num1 and big>num3):
+    print(num2)
+else:
+    print(num3)
+
